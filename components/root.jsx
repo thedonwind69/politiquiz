204b62b0bc886903bc81722dc3e51ed7208705a7
Don Nguyen <thedonwind69@gmail.com>
2018-06-03 18:50:00 -0700

    first add

---
+++ components/root.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Root extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render () {
+        return (
+            <div>
+                <h1>Politiquiz, bitch</h1>
+            </div>
+        )
+    }
+
+}
+
+export default Root;
