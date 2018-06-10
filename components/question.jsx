9f28970b4822ec692f63843893cd9b4f5595b6b4
Don Nguyen <thedonwind69@gmail.com>
2018-06-09 21:22:39 -0700

    add

---
+++ components/question.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+    Route,
+    Redirect,
+    Switch,
+    Link,
+    HashRouter
+} from 'react-router-dom';
+
+class Question extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render () {
+        return (
+            <div>
+          
+            </div>
+        )
+    }
+
+}
+
+export default Question;
