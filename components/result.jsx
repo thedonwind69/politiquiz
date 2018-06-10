fd7e2cb7e18e47812870afcc2e4f24ae7dda505a
Don Nguyen <thedonwind69@gmail.com>
2018-06-10 16:52:02 -0700

    add

---
+++ components/result.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+    Route,
+    Redirect,
+    Switch,
+    Link,
+    HashRouter
+} from 'react-router-dom';
+
+class Result extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            resultReady: false
+        }
+    }
+
+    componentDidMount () {
+        setTimeout(function() { //Start the timer
+            this.setState({resultReady: true}) //After 1 second, set render to true
+        }.bind(this), 5000)
+    }
+
+    render () {
+        if (this.state.resultReady) {
+            return (
+                <div class='fade'>
+                    <h1>Result bitch!!!</h1>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    <div class="loader">
+                    
+                    </div>
+                 <h1>Loading bitch...</h1>
+                </div>
+            )
+        }
+       
+    }
+
+
+
+    
+}
+
+export default Result;
